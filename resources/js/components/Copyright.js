6cf82b16424bfceefa42a54254390a0ca2e1ee2b
Alvis Logins <allogn@gmail.com>
2020-06-04 21:15:44 +0200

    Copyright component, tests passed

--- resources/js/components/Copyright.js
+++ resources/js/components/Copyright.js
@@ -2,13 +2,14 @@
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
 import Link from '@material-ui/core/Link';
+import config from 'react-global-configuration';
 
 function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
       {'Copyright © '}
-      <Link color="inherit" href="https://localhost/">
-        My App
+      <Link color="inherit" href={ config.get('base_url') }>
+      { config.get('app_name') }
       </Link>{' '}
       {new Date().getFullYear()}
       {'.'}
--- resources/js/components/__tests__/copyright.test.js
+++ resources/js/components/__tests__/copyright.test.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
+import config from 'react-global-configuration';
+import { render } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
 import Copyright from '../Copyright';
 
-test('Test correct year in copyright', () => {
-    const component = renderer.create(
+test('Test correct year and app name in copyright', () => {
+    config.set({ app_name: 'app-name', base_url: "https://localhost/" });
+    const { getByText } = render(
         <Copyright></Copyright>
     );
-    let tree = component.toJSON();
-    expect(tree).toMatchSnapshot();
-    console.log(tree)
+    expect(getByText('app-name')).toBeInTheDocument();
+    expect(getByText('2020', {exact: false})).toBeInTheDocument();
 });
